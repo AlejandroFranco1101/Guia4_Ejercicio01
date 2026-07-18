554aabcb6feca3dadd656e804c98e8ffec41ea91
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-18 13:52:59 -0600

    Permiso Denegado
    Se comprueba que en el log del sistema que el que
    permiso para grabar audio esta denegado.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.example.guia4"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
--- app/src/main/java/com/example/guia4/MainActivity.kt
+++ app/src/main/java/com/example/guia4/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.guia4
-
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
+    private val TAG = "Guia4-Permisos"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Comprobación de permisos
+        comprobarEstadoPermiso()
+    }
+    // 1. Comprobar estado permiso
+    private fun comprobarEstadoPermiso() {
+        val estadoPermiso = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.RECORD_AUDIO
+        )
+        if (estadoPermiso != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permiso para grabar denegado")
         }
     }
+    // 2. Configurar permisos
+    // 3. Solicitud de permiso
+    // 4. Manejo de respuesta de solicitud de permiso
 }
